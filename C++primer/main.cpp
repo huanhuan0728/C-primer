d44b8792f88e73a75b48fed18ffab180ec63d7ef
xuhuan lu <1477405138@qq.com>
2023-11-17 10:47:17 +0800

    Initial Commit

---
+++ C++primer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++primer
+//
+//  Created by xuhuan lu on 2023/11/17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
