5f380b9879cba0fd393aad7fbf7baee897c8acb9
xuhuan lu <1477405138@qq.com>
2023-11-17 11:05:35 +0800

    添加了头文件，以后的函数大多都会写在function.cpp文件中

---
+++ function.cpp
@@ -0,0 +1,8 @@
+//
+//  function.cpp
+//  C++primer
+//
+//  Created by xuhuan lu on 2023/11/17.
+//
+
+#include "function.hpp"
---
+++ function.hpp
@@ -0,0 +1,13 @@
+//
+//  function.hpp
+//  C++primer
+//
+//  Created by xuhuan lu on 2023/11/17.
+//
+
+#ifndef function_hpp
+#define function_hpp
+
+#include <stdio.h>
+
+#endif /* function_hpp */
