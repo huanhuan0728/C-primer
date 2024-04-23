ab3254842e4bc63918d5501f18146147e44866ea
xuhuan lu <102048940+huanhuan0728@users.noreply.github.com>
2024-04-23 22:31:03 +0800

    Create stack.c

---
+++ 数据结构复习/StackQueueArray/stack.c
@@ -0,0 +1,56 @@
+//
+//  stack.c
+//  StackQueueArray
+//
+//  Created by xuhuan lu on 2024/4/23.
+//
+
+#include "stack.h"
+
+void printf_hello(void){
+    printf("hello");
+}
+
+// 栈的基本操作，，解算法题时，若题干未作出限制，则可以直接使用这些基本的操作函数
+// 初始化一个栈
+void InitStack(SqStack *S){
+    S->top = -1;
+}
+
+// 判断一个栈是否为空，若栈S为空返回true，否则返回false
+bool StackEmpty(SqStack *S){
+    if (S->top == -1)
+        return true;
+    else
+        return false;
+}
+
+// 进栈，若栈S未满，则将x加入使之成为新栈顶
+bool Push(SqStack *S, ElemType x){
+    if (S->top == MaxSize-1) {
+        return false;
+    }
+    S->data[++S->top] = x;
+    return true;
+}
+
+// 出栈，若栈S非空，则弹出栈顶元素，并用x返回
+bool Pop(SqStack *S, ElemType *x){
+    if (S->top == -1) {
+        return false;
+    }
+    *x = S->data[S->top];
+    return true;
+}
+
+// 读栈顶元素，但不出栈，若栈s非空，则用x返回栈顶元素
+bool GetTop(SqStack *S, ElemType *x){
+    if (S->top == -1) {
+        return false;
+    }
+    *x = S->data[S->top];
+    return true;
+}
+
+// 销毁栈，并释放栈S占用的储存空间
+bool DestroyStack(SqStack *s);
